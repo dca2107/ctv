42493eadcb934ca726dae76f0f3c8783af801f4f
fasta <146788352+dca2107@users.noreply.github.com>
2024-09-20 13:07:18 -0600

    Update SV-23-TVOHD.php

--- hls/SV-23-TVOHD.php
+++ hls/SV-23-TVOHD.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/vnd.apple.mpegurl');
 
-// Directorio donde se almacenan los M3U8 temporales
-$path = "https://conceptoweb-studio.com/radio/video/tvo/";
+// Directorio local donde se almacenan los M3U8 temporales
+$path = "/ctv/hls/SV-23-TVO.m3u8"; // Ruta local en el servidor (no URL HTTP)
 $files = glob($path . "*.m3u8");
 
 echo "#EXTM3U\n";
@@ -10,6 +10,6 @@ echo "#EXT-X-VERSION:3\n";
 
 foreach ($files as $file) {
     echo "#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1920x1080,CODECS=\"avc1.640029,mp4a.40.2\"\n";
-    echo "https://github.com/dca2107/ctv/blob/main/hls/SV-23-TVO.m3u8" . basename($file) . "\n";
+    echo "https://conceptoweb-studio.com/radio/video/tvo/" . basename($file) . "\n"; // URL completa correcta
 }
 ?>
